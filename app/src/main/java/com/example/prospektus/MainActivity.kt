3739a0290dfbc88844c611787e49abc639933670
bertayyonel <bertayyonel@gmail.com>
2020-11-30 21:53:05 +0300

    aaaaaaaaaaaaaaa

--- app/src/main/java/com/example/prospektus/MainActivity.kt
+++ app/src/main/java/com/example/prospektus/MainActivity.kt
@@ -9,5 +9,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var y = 31
         var a = 5
+        var celil1 = "hacı şaka mısın"
+        var celil2 = "hacı şaka mısın"
+        var celil3 = "hacı şaka mısın"
+        var v = "var"
+        var v1 = "var"
+        var v2 = "var"
+        var v3 = "var"
+        var v4 = "var"
+        var v5 = "var"
+
     }
 }
